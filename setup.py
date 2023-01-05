6368782af7a2abeaa6820fc9052ebccc733b8c1f
dinbaba <s204075@student.dtu.dk>
2023-01-05 19:46:17 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project work for course 02476 Machine Learning Operations.',
+    author='Magnus Guldberg Pedersen',
+    license='MIT',
+)
