a670808626602b9dc76a304597dd8194b267e608
dinbaba <s204075@student.dtu.dk>
2023-01-12 13:50:21 +0100

    added model source file

---
+++ src/models/model.py
@@ -0,0 +1,26 @@
+from torch import nn, optim
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # define the layers
+        # TODO: update the layers with the size of the input
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+        # add dropout layer
+        self.dropout = nn.Dropout(p=0.2)
+        
+    def forward(self, x):
+        # make sure input tensor is flattened
+        x = x.view(x.shape[0], -1)
+        
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        
+        x = F.log_softmax(self.fc4(x), dim=1)
+        
+        return x
