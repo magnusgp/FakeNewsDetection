4fed539ddb5a1e95be1c0a178079504e6991c037
magnusgp <s204075@student.dtu.dk>
2023-01-17 11:29:17 +0100

    added testing of data and training

---
+++ tests/test_training.py
@@ -0,0 +1,31 @@
+import torch
+from torch import optim, nn
+from src.models.model import Model
+from tqdm import tqdm
+import pytest
+import os
+
+@pytest.mark.skipif(not os.path.exists('data/processed/trainset.pt'), reason="Training files not found")
+@pytest.mark.parametrize("lr,criterion", [(1e-3, nn.NLLLoss()), (1e-4, nn.CrossEntropyLoss()), (1e-5, nn.NLLLoss())])
+def test_training(lr, criterion):
+    epochs = 5
+    model = Model()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion = criterion
+    # load trainset from data folder
+    trainset = torch.load('data/processed/trainset.pt')
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+    training_loss = []
+
+    for e in range(epochs):
+        running_loss = 0
+        for images, labels in tqdm(trainloader):
+            optimizer.zero_grad()
+            log_ps = model(images)
+            loss = criterion(log_ps, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            # check that the training loss is not negative
+            assert running_loss > 0, "Training loss is negative"
+            break
