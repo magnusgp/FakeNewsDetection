f5cb1f82360f3b9e6fb9fd2a1e0914b2295efe65
magnusgp <s204075@student.dtu.dk>
2023-01-19 11:29:39 +0100

    updated api

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, HTTPException
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 from joblib import load
 import os
+from fastapi.responses import HTMLResponse
 
 # Load the model
 model_path = "models/roberta-base/checkpoint-30/"
@@ -17,11 +18,40 @@ app = FastAPI()
 # Define the default route 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Fake News Detector API"}
+    return HTMLResponse(content="""<html>
+    <body>
+    <h1>Welcome to the Fake News Detector API</h1>
+    <a href="/input_text/">Enter your text message here</a>
+    </body>
+    </html>""")
+
+@app.get("/input_text/")
+def input_text():
+    return HTMLResponse(content="""<html>
+    <body>
+    <h1>Enter your text message here:</h1>
+    <form action="/predict_if_fake_news/" method="get">
+    <input type="text" name="text_message">
+    <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>""")
 
 # Define the route to the sentiment predictor
-@app.post("/predict_if_fake_news/")
+@app.get("/predict_if_fake_news/")
 def predict(text_message):
+    # make the text message a string
+    text_message = str(text_message)
+    # remove all question marks and percentage signs
+    text_message = text_message.replace("%", " ")
+    text_message = text_message.replace("?", "")
     model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_path)
     classify = pipeline("text-classification", model=model_path, tokenizer=tokenizer)
-    return classify(text_message)
+    # return htmlresponse that presents both the text message and the prediction
+    return HTMLResponse(content="""<html>
+    <body>
+    <h1>Text Message: {}</h1>
+    <h1>Prediction: {}</h1>
+    </body>
+    </html>""".format(text_message, classify(text_message)[0]['label']))
+    #return classify(text_message)
