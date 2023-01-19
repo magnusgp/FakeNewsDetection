982a817557acd3f6707b4046aec412766e4ac961
Aeyran-Dapouyeh <ariandapoyeh@gmail.com>
2023-01-19 17:31:24 +0100

    Fix path.

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -47,8 +47,10 @@ def train(config):
 
     # Preparing model
     # dataset = torch.load('data/processed/dataset.pt')
-    print(r"{}/data/processed/dataset.csv".format(os.chdir(os.path.abspath(os.path.abspath(os.path.abspath(os.path.abspath(os.getcwd())))))))
-    dataset = torch.load(r"{}/data/processed/dataset.csv".format(os.chdir(os.path.abspath(os.path.abspath(os.path.abspath(os.path.abspath(os.getcwd())))))))
+    print(os.getcwd())
+    print(os.path.abspath(os.getcwd()))
+    print(r"{}/data/processed/dataset.csv".format((os.path.abspath(os.path.abspath(os.path.abspath(os.path.abspath(os.getcwd())))))))
+    dataset = torch.load(r"{}/data/processed/dataset.csv".format((os.path.abspath(os.path.abspath(os.path.abspath(os.path.abspath(os.getcwd())))))))
     trainset = dataset["train"]
     trainset = (
         trainset.remove_columns(["text"])
